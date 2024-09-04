debac7057e508d0148aa595a2016fc61b871af10
isak-mps <isakmarais4@gmail.com>
2024-09-04 15:53:06 +0200

    public/profile1.png

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -19,8 +19,8 @@ function Hero() {
             className='h-[100px] object-cover rounded-full absolute right-16 bottom-32' />
         </div>
         <div className='text-center max-w-3xl'>
-        <h2 className='font-bold text-[60px] text-slate-700'>Easy scheduling ahead</h2>
-        <h2 className='text-xl mt-5 text-slate-500'>Scheduly is your scheduling automation platform for eliminating the back-and-forth emails to find the perfect time — and so much more.</h2>
+        <h2 className='font-bold text-[60px] text-slate-700'>Two Brothers Transport</h2>
+        <h2 className='text-xl mt-5 text-slate-500'>This is your delivery scheduling platform for eliminating the back-and-forth whatsapps to find the perfect time — and so much more.</h2>
         <div className='flex gap-4 flex-col mt-5'>
             <h3 className='text-sm'>Sign Up free with Google and Facebook</h3>
             <div className='flex justify-center gap-8'>
